a55c148356c027c9ff46b0b232513e915a7f064a
Michał Gurgul <m.gurgul@AKE.LOCAL>
2019-09-03 15:19:23 +0200

    merge two csv and remove duplicates

--- diff.py
+++ diff.py
@@ -1,11 +1,11 @@
 import pandas
 
 names = ['IDX','Price','City', 'Region', 'Model', 'Year', 'Mileage', 'Displacement', 'Petrol', 'Start', 'Duration', 'EndPrice']
-df1 = pandas.read_csv('dataframe.csv',skiprows = 1, 
+df1 = pandas.read_csv('_ALL.csv',skiprows = 1, 
                   index_col=False, 
                   names = names, 
                   encoding='utf-8')
-print(df1)
+#print(df1)
 
 
 i=0
@@ -16,7 +16,12 @@ df2 = pandas.read_csv('_NEW.csv',skiprows = 1,
                   encoding='utf-8')
 
 df1["Duration"].fillna(0) 
-print (df1.dtypes)
+#print (df1.dtypes)
+
+df3 = pandas.DataFrame(pandas.concat([df1, df2], ignore_index=True))
+df3 = df3.drop_duplicates(subset=['IDX'])
+
+print(df3) 
 
 for counter, row in df1.iterrows():
     df1_IDX = row['IDX']
@@ -24,12 +29,12 @@ for counter, row in df1.iterrows():
 
     for index, row in df2.iterrows():
         if (df1_IDX == row['IDX']):
-            print(df1.iloc[i-1,10]) 
+            #print(df1.iloc[i-1,10]) 
             
             df1.at[i-1, 'Duration'] = df1.iloc[i-1,10] + 1 # increase day counter
             df1.at[i-1, 'EndPrice'] = df2.iloc[index,1] # assign last price from today file
 
 
-export_csv = df1.to_csv (r'.\dataframe.csv', index = None, header=True, encoding="utf-8") #Don't forget to add '.csv' at the end of the path
+export_csv = df3.to_csv (r'.\dataframe.csv', index = None, header=True, encoding="utf-8") #Don't forget to add '.csv' at the end of the path
 
-print(df1)
+#print(df1)
