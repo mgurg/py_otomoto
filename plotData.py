7cf04b3ebc7912ce598f66f464a9dda6b489f763
mgurg <34517378+mgurg@users.noreply.github.com>
2020-01-24 19:02:23 +0100

    Creat plotData
    Initial code for image generaration

---
+++ plotData.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+data = pd.read_csv('./my_df.csv')
+
+data.hist(bins=50,figsize=(20,15))
+
+plt.savefig('foo.png', bbox_inches='tight')
+#plt.savefig('foo.pdf')
